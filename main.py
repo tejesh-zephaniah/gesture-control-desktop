0186a8ef11d19aac5faf88057779381f481ce7c7
tejesh-zephaniah <tejeshyellamilli@zohomail.in>
2026-03-22 20:09:59 +0530

    implement multi-finger detection using mediapipe landmarks

--- main.py
+++ main.py
@@ -2,10 +2,12 @@
 import cv2
 from camera.camera_manager import CameraManager
 from vision.hand_detector import HandDetector
+from vision.landmark_processor import LandmarkProcessor
 import mediapipe as mp
 
 camera= CameraManager()
 vision= HandDetector()
+processor = LandmarkProcessor()
 
 while True:
     ret, frame = camera.read_frame()
@@ -15,6 +17,12 @@ while True:
         break
     
     results = vision.process(frame)
+    landmarks = processor.extract_landmarks(results, frame.shape)
+    fingers = processor.get_finger_states(landmarks)
+    
+    if landmarks:
+        print(fingers)
+        
     vision.draw(frame,results)
             
     cv2.imshow("Gesture-Control-Desktop", frame)
